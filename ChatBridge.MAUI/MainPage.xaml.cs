2d53802fa5726b3bb557517c24dc129c026d0579
AbdumajidovAvazbek <abdumajidova51@gmail.com>
2023-11-16 10:50:47 -0800

    a

---
+++ ChatBridge.MAUI/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ChatBridge.MAUI
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ChatBridge.MAUI/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ChatBridge.MAUI
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
