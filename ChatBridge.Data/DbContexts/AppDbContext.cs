d667e6f925a0b531953ff56e8ef3adf1d94ff2b9
Muhammadziyo Mahkamov <127393000+Ziyobek617@users.noreply.github.com>
2023-11-19 21:36:20 +0500

    Data half finished

---
+++ ChatBridge.Data/DbContexts/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using ChatBridge.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace ChatBridge.Data.DbContexts
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<UserAsset> UsersAsset { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+        }
+    }
+}
